9e92416ef5875e80810c11d6854603fd5edcc877
Fazyl2023 <khusaenov.fazyl@mail.ru>
2023-10-11 17:51:14 +0300

    modified:   fapi.py 	modified:   main.py 	new file:   winRed.csv

--- fapi.py
+++ fapi.py
@@ -1,24 +1,32 @@
 import uvicorn
 from fastapi import FastAPI
-from main import lap_Model, lap_Species
+from main import win_Model, win_quaility
 
 # 2. Create the app object
 app = FastAPI()
-model = lap_Model()
+model = win_Model()
 
 
 @app.post('/predict')
-def predict_species(iris: lap_Species):
-    data = iris.dict()
-    prediction = model.predict_species(
-        data['Model_Name'], data['Category'], data['Operating_System_Version']
+def predict_quality(alc: win_quaility):
+    data = alc.dict()
+    prediction, probablity = model.predict_quality(
+        data['fixed_acidity'], data['volatile_acidity'], data['citric_acid'], 
+        data['residual_sugar'], data['chlorides'], data['free_sulfur_dioxide'], data['total_sulfur_dioxide'],
+        data['density'], data['pH'], data['sulphates'], data['alcohol']
+        #fixed_acidity,volatile_acidity,citric_acid,residual_sugar,chlorides,free_sulfur_dioxide,total_sulfur_dioxide,density,pH,sulphates,alcohol
     )
     return {
-        'company': prediction
+        'Оценка вина': str(prediction),
+        'вероятность' : probablity
+
     }
 
-
+#7.1,0.35,0.29,2.5,0.096,20.0,53.0,0.9962,3.42,0.65,11.0 
 # 5. Run the API with uvicorn
+#uvicorn fapi:app --reload
 #Отображать в реальном времени
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000) 
+    uvicorn.run("fapi:app", host="127.0.0.1", port=5000, reload=True, access_log=False)
+
+
--- main.py
+++ main.py
@@ -2,24 +2,33 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from pydantic import BaseModel
 import joblib
+from sklearn import datasets
 
 
-class lap_Species(BaseModel):
-    Model_Name: str 
-    Category: str 
-    Operating_System_Version: str 
-
+class win_quaility(BaseModel):
+    fixed_acidity: float 
+    volatile_acidity: float 
+    citric_acid: float 
+    residual_sugar: float
+    chlorides: float
+    free_sulfur_dioxide: float 
+    total_sulfur_dioxide: float
+    density: float
+    pH: float
+    sulphates: float 
+    alcohol: float
+    #fixed_acidity,volatile_acidity,citric_acid,residual_sugar,chlorides,free_sulfur_dioxide,total_sulfur_dioxide,density,pH,sulphates,alcohol
 
 
 # 3. Class for training the model and making predictions
-class lap_Model:
+class win_Model:
     # 6. Class constructor, loads the dataset and loads the model
     #    if exists. If not, calls the _train_model method and 
     #    saves the model
     def __init__(self):
-        self.df = pd.read_csv('laptops_train.csv')
+        self.df = pd.read_csv('winRed.csv')
         
-        self.model_fname_ = 'save.pkl'
+        self.model_fname_ = 'model.pkl'
         try:
             self.model = joblib.load(self.model_fname_)
         except Exception as _:
@@ -29,8 +38,8 @@ class lap_Model:
 
     # 4. Perform model training using the RandomForest classifier
     def _train_model(self):
-        X = self.df.drop('Manufacturer', axis=1)
-        y = self.df['Manufacturer']
+        X = self.df.drop('quality', axis=1)
+        y = self.df['quality']
         rfc = RandomForestClassifier()
         model = rfc.fit(X, y)
         return model
@@ -38,8 +47,9 @@ class lap_Model:
 
     # 5. Make a prediction based on the user-entered data
     #    Returns the predicted species with its respective probability
-    def predict_species(self, Model_Name, Category, Operating_System_Version):
-        data_in = [[Model_Name, Category, Operating_System_Version, Operating_System_Version]]
+    def predict_quality(self, fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides, free_sulfur_dioxide,total_sulfur_dioxide, density, pH, sulphates, alcohol):
+        data_in = [[fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides, free_sulfur_dioxide,total_sulfur_dioxide, density, pH, sulphates, alcohol]]
+        #prediction = self.model.predict(data_in)
         prediction = self.model.predict(data_in)
-        
-        return prediction['this']
+        probability = self.model.predict_proba(data_in).max()
+        return prediction[0], probability
